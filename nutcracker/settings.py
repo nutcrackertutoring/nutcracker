a2044b74ab3c885d537953619d750c028a8b4bf1
Nathan <nathan.goldwaser@gmail.com>
2018-05-08 10:11:58 +1000

    Backup before Heroku

--- nutcracker/settings.py
+++ nutcracker/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='jh1yf91)^@^fjnq#qaf2xtces_*rs2juo@&i$t2hd%24d^a!9$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
